9bbb26631216e2d1a23213596567d46157a3e108
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-10-28 20:13:31 -0300

    Conhecendo Array

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] idades = new int[5] { 15,28,30,50,45};
+
+            int acumulador = 0;
+            for (int i = 0; i < idades.Length; i++)
+            {
+                acumulador += idades[i];
+            }
+           int media = acumulador / idades.Length;
+
+            Console.WriteLine(media);
+            Console.ReadLine();
+        }
+    }
+}
